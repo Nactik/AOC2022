0994f27890dad6635c8578ea18a63fb72117f306
Nathan Capiaux <nathan.capiaux@sia-partners.com>
2022-12-01 10:34:20 +0100

    part 2

--- day1/part2.py
+++ day1/part2.py
@@ -1,2 +1,22 @@
-with open("input.txt", "r") as f:
+with open('input.txt', "r") as f:
     # Do something here
+    content = f.read().splitlines()
+    inventory = []
+
+    elf_id = 0
+    calories = 0
+
+    for line in content :
+        if line == "":
+            inventory.append(calories)
+            calories=0
+            elf_id += 1
+        else:
+            calories += int(line)
+
+    inventory.append(calories) ## Append last count
+
+    inventory.sort(reverse=True) ## Sort the list
+    top_three = inventory[:3] ## Get the top threes
+
+    print(sum(top_three))
