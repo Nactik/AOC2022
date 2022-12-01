c55419736c05aebb178cb08c507eec60aa4ec6dc
Nathan Capiaux <capiauxnathan@gmail.com>
2022-12-01 09:27:39 +0100

    part1 done

--- day1/part1.py
+++ day1/part1.py
@@ -1,3 +1,17 @@
 with open('input.txt', "r") as f:
     # Do something here
-    content = f.readlines()
+    content = f.read().splitlines()
+    inventory = []
+
+    elf_id = 0
+    sum = 0
+
+    for line in content :
+        if line == "":
+            inventory.append(sum)
+            sum=0
+            elf_id += 1
+        else:
+            sum += int(line)
+
+    print(max(inventory))
