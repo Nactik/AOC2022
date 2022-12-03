43b7b8c82f901d07827708492d4bfc77abad1503
Nathan Capiaux <capiauxnathan@gmail.com>
2022-12-03 14:35:18 +0100

    part1

---
+++ day3/part1.py
@@ -0,0 +1,21 @@
+with open("input.txt", "r") as f:
+
+    content = f.read().splitlines()
+
+    # Do something here
+    items = [
+        [line[: len(line) // 2], line[len(line) // 2 :]] for line in content
+    ]
+
+    total = 0
+
+    for items1, items2 in items:
+        # Find common letter
+        common = [letter for letter in items1 if letter in items2][0]
+        priority = priority = (
+            ord(common) - 96 if common.islower() else ord(common) - 38
+        )
+
+        total += priority
+
+    print(f"Total : {total}")
