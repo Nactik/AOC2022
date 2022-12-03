741af723c3567cee024b37f11f77ef97a9d16dfc
Nathan Capiaux <capiauxnathan@gmail.com>
2022-12-03 01:56:03 +0100

    Linted part 2

--- day2/part2.py
+++ day2/part2.py
@@ -1,33 +1,32 @@
 SCORE = {
-    "A": 1, # Rock
-    "B": 2, # Paper
-    "C": 3, # Scissors
+    "A": 1,  # Rock
+    "B": 2,  # Paper
+    "C": 3,  # Scissors
 }
 
 RESULT = {
-    "X": 0, # Lose
-    "Y": 3, # Draw
-    "Z": 6, # Win
+    "X": 0,  # Lose
+    "Y": 3,  # Draw
+    "Z": 6,  # Win
 }
 
 
 def _get_move_score(choice1: str, expected_result: int) -> int:
 
-    if expected_result == 3: # Draw
+    if expected_result == 3:  # Draw
         # print(f"{SCORE[choice1]} {result} - I should choose {SCORE.get(choice1, 0)}")
         return SCORE.get(choice1, 0)
-  
-    if expected_result == 0: # Lose
+
+    if expected_result == 0:  # Lose
         # print(f"{SCORE[choice1]} {result} - I should choose {(SCORE.get(choice1, 0) + 2) % 3} ")
-        return (SCORE.get(choice1, 0) + 1) % 3 + 1 
-  
+        return (SCORE.get(choice1, 0) + 1) % 3 + 1
+
     # print(f"{SCORE[choice1]} {result} - I should choose {(SCORE.get(choice1, 0) + 1) % 3} ")
     return (SCORE.get(choice1, 0)) % 3 + 1
 
 
-with open('input.txt', "r") as f:
+with open("input.txt", "r") as f:
 
-    
     # Do something here
     content = f.read().splitlines()
     games = [line.split() for line in content]
@@ -39,6 +38,6 @@ with open('input.txt', "r") as f:
         move_score = _get_move_score(choice1, result)
 
         # print(f"{choice1} {result} - Game score : {move_score + result}")
-        score += (move_score + result)
-        
+        score += move_score + result
+
     print(f"Total score: {score}")
